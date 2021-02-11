254af73b527a6d860b2a4d089755ff55c36d6a0e
gamingbeast36 <64628398+gamingbeast36@users.noreply.github.com>
2021-02-11 22:14:33 +0530

    Create perm.sh

---
+++ i3blocks/perm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+chmod +x ~/.config/i3blocks/battery/battery_info.sh
+chmod +x ~/.config/i3blocks/cpu/cpu_info.sh
+chmod +x ~/.config/i3blocks/sound/sound_info.sh
+chmod +x ~/.config/i3blocks/sound/sound_burst.sh
+chmod +x ~/.config/i3blocks/weather/weather_info.sh
+chmod +x ~/.config/i3blocks/weather/weather.py
+
