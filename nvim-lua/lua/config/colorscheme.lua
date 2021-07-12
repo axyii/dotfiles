098c51776e270fae2b7fb1be29aa5159d11df61c
gamingbeast36 <64628398+gamingbeast36@users.noreply.github.com>
2021-07-12 19:56:01 +0530

    Add files via upload

---
+++ nvim-lua/lua/config/colorscheme.lua
@@ -0,0 +1,19 @@
+local utils = require('utils')
+
+utils.opt('o', 'termguicolors', true)
+
+
+--Tokyonight
+vim.g.tokyonight_style = "storm" --Available storm,night,day
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
+-- Load the colorscheme
+vim.cmd[[colorscheme tokyonight]]
+
+--OneDark
+--vim.g.onedark_style = 'deep' --Available dark,darker,cool,deep,warm,warmer
+--vim.cmd[[colorscheme onedark]]
