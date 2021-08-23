499305096cd0a8a82253bd657344de5eb30cb332
gamingbeast36 <64628398+gamingbeast36@users.noreply.github.com>
2021-08-23 11:45:00 +0530

    Update statusline.lua

--- nvim-lua/lua/config/statusline.lua
+++ nvim-lua/lua/config/statusline.lua
@@ -22,13 +22,6 @@ require'lualine'.setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { require'tabline'.tabline_buffers },
-    lualine_x = { require'tabline'.tabline_tabs },
-    lualine_y = {},
-    lualine_z = {},
-  },
+  tabline = {},
   extensions = {}
 }
